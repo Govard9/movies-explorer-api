6dcaaee05585cb46d90469a234ab5caa36e793f9
govard9 <web-supsu@yandex.ru>
2023-06-12 11:23:06 +0700

    feat: Внести правки по чек-листу.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 require('dotenv').config();
-const router = require('./routes/index');
 const { DB_CONNECTION_STRING } = process.env;
+const router = require('./routes/index');
+
+const { LIMITER_OPTIONS } = require('./configurations');
+const limiter = rateLimit(LIMITER_OPTIONS);
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
@@ -12,20 +16,21 @@ const app = express();
 
 const handleErrors = require('./middlewares/handleErrors');
 
+app.use(limiter);
 app.use(helmet());
 
 app.use(express.json());
 app.use(requestLogger); // логгер запросов
 app.use(router);
 app.use(errorLogger); // логгер ошибок
+
 mongoose.connect(DB_CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
   console.log('Connected to database.');
 }).catch((error) => {
-  console.error('Error connecting to database:', error);
-});
+  console.error('Error connecting to database:', error);});
 
 app.use(errors()); // обработчик ошибок celebrate
 
